e6695f4817e8d3730f74001b9c5433e4f524b814
Dey11subrata <deysubrata2311@gmail.com>
2023-10-19 23:35:19 +0530

    comments added regarding functions

--- service/encryption.js
+++ service/encryption.js
@@ -1,11 +1,13 @@
 const bcrypt = require("bcryptjs");
 
+// @function to encrypt password before saving it in database
 const encryptPassword = (userPassword) => {
   const salt = bcrypt.genSaltSync(8);
   const hashedPassword = bcrypt.hashSync(userPassword, salt);
   return hashedPassword;
 };
 
+// @function to decrypt password and match with the password provided by user to access account
 const decryptedPassword = (userPassword, storedPassword) => {
   console.log(userPassword, storedPassword);
   // find user by email first then match password
--- service/loginService.js
+++ service/loginService.js
@@ -1,12 +1,18 @@
 const User = require("../model/User");
 const { decryptedPassword } = require("./encryption");
 
+// @function to check is user registered corresponding to provided email
+// @required when authenticating user
+// @result of this function allow user to login or not to login
 const lookForUser = async (userEmail) => {
   const result = await User.findOne({ userEmail: userEmail });
   console.log("lookForUser:---", result);
   return result;
 };
 
+// @function to allow user to loggin
+// @check user is registered or not
+// @for registered user match the encrypted password with provided password and then, either allow to enter or not allow to enter.
 const userLogin = async (userCredential) => {
   console.log(userCredential);
   const user = await lookForUser(userCredential.userEmail);
