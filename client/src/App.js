4d98e6a1a934b9aeb15cc27fcfd57ac9d6154702
Dey11subrata <deysubrata2311@gmail.com>
2023-10-23 20:14:12 +0530

    User Registration Layout created

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import UserLogin from "./components/Registration_Login/Login/UserLogin";
+import UserRegistration from "./components/Registration_Login/Registration/UserRegistration";
 
 function App() {
   return (
     <div className="App">
       <h2>Navigation Bar</h2>
       <UserLogin />
+      <UserRegistration />
     </div>
   );
 }
